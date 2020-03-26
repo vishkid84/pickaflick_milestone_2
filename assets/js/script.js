4eb37b387c67dfd2de8aa365acfcb345e334f598
vishkid84 <vishalittr@gmail.com>
2020-03-26 21:28:46 +0000

    Added html to js to return random movie from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 const APIKey = "a633b83aa763a0e8fad2c80cc66c54b9";
-const img_URL = "https://image.tmdb.org/t/p/w500/"
+const imgURL = "https://image.tmdb.org/t/p/w185/"
 
 const searchButton = document.getElementById("pickflickBtn");
 // const searchMovie = document.getElementById("searchMovie");
 
 const url = "http://api.themoviedb.org/3/";
+
+// function pickaflickContainer{
+
+// }
 
 
 $(searchButton).click(function () {
@@ -18,9 +22,24 @@ $(searchButton).click(function () {
     .then(function (data) {
         let movies = data.results;
         const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+        var imgSource = imgURL + randomMovie.poster_path;
         let el = document.getElementById("randomMovieResult");
             el.innerHTML = "";
-            el.innerHTML = `<p>${randomMovie.title}</p>`;
+            el.innerHTML = `<div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="media">
+                        <img class="mr-3 align-self-start" src="${imgSource}">
+                        <div class="media-body movie-info">
+                            <h5 class="movie-title">${randomMovie.title}</h5>
+                            <p class="movie-rating">IMDB Rating: ${randomMovie.vote_average}</p>
+                            <p class="movie-rating">${randomMovie.overview}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
         console.log("randomMovie", randomMovie);
     })
     .catch(function (error) {
