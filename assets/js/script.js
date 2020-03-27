517a1b66bf24bbd1b3fbc8796f031747e16db6ca
vishkid84 <vishalittr@gmail.com>
2020-03-27 13:08:28 +0000

    Added script to target genre dropdown and filter by id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,19 @@
 const APIKey = "a633b83aa763a0e8fad2c80cc66c54b9";
-const imgURL = "https://image.tmdb.org/t/p/w185/"
+const imgURL = "https://image.tmdb.org/t/p/w185/";
 
 const searchButton = document.getElementById("pickflickBtn");
 // const searchMovie = document.getElementById("searchMovie");
 
 const url = "http://api.themoviedb.org/3/";
 
-// function randomMovieGenre(randomMovie){
-    
-// }
 
+// Render to html
 function pickaflickContainer(randomMovie){
     let movieElement = document.getElementById("randomMovieResult");
     let movieTitle = randomMovie.title;
     let movieRating = randomMovie.vote_average;
     let movieOverview = randomMovie.overview;
-    let movieGenre = randomMovie.genre_ids;
-        
-        // if (document.getElementById("genre").value == "Action"){
-        //     movieGenre.map(function (genre){
-        //         let newGenre = genre === 18;
-        //         alert (newGenre);
-        //         randomMovie = randomMovie.newGenre;
-        //     })
-        // };
+    // let movieGenre = randomMovie.genre_ids;
 
     pickflickaContent = 
     `<div class="container">
@@ -57,12 +47,44 @@ $(searchButton).click(function () {
     })
     .then(function (data) {
         let movies = data.results;
-        let randomMovie = movies[Math.floor(Math.random() * movies.length)];
+        let actionMovies = data.results.filter(function(movies){
+                return movies.genre_ids.includes(28);
+            })
+        let comedyMovies = data.results.filter(function(movies){
+                return movies.genre_ids.includes(35);
+            })
+        let dramaMovies = data.results.filter(function(movies){
+                return movies.genre_ids.includes(18);
+            })
+
+        // Taget the genre dropdown and filter by genre id in the API 
+        if (document.getElementById("genre").value == "Action") {
+             randomMovie = actionMovies[Math.floor(Math.random() * actionMovies.length)];
+        }
+        else if (document.getElementById("genre").value == "Comedy") {
+            randomMovie = comedyMovies[Math.floor(Math.random() * comedyMovies.length)];
+        }
+        else if (document.getElementById("genre").value == "Drama") {
+            randomMovie = dramaMovies[Math.floor(Math.random() * dramaMovies.length)];
+        }
+        else{
+            randomMovie = movies[Math.floor(Math.random() * movies.length)];
+        }
+        
         console.log("randomMovie", randomMovie);
-            pickaflickContainer(randomMovie);
+        pickaflickContainer(randomMovie);
+        alert (randomMovie.genre_ids);
     })
     .catch(function (error) {
         console.error("Something has gone wrong");
         console.error(error);
     })
 })
+
+// function randomMovieGenre(randomMovie){
+//     if (document.getElementById("genre").value == "Action") {
+//         data.results.filter(function(movies){
+//             return movies.genre_ids.includes(28);
+//         })
+//     }
+// }
