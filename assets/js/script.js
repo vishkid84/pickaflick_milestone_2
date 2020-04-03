9b9eee99b49ed271a3a983d7c7fb5c0480b25020
vishkid84 <vishalittr@gmail.com>
2020-04-03 20:22:05 +0000

    Removed rating, added background color change on each selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,15 @@ const url = "http://api.themoviedb.org/3/";
 
 
 // Render to html
-function pickaflickContainer(randomMovie){
+function pickaflickContainer(randomMovie) {
     let movieElement = document.getElementById("randomMovieResult");
     let movieTitle = randomMovie.title;
     let movieRating = randomMovie.vote_average;
     let movieOverview = randomMovie.overview;
     // let movieGenre = randomMovie.genre_ids;
 
-    pickflickaContent = 
-    `<div class="container">
+    pickflickaContent =
+        `<div class="container">
             <div class="row">
                 <div class="col">
                     <div class="media">
@@ -31,8 +31,8 @@ function pickaflickContainer(randomMovie){
             </div>
         </div>
         `;
-            movieElement.innerHTML = "";
-            movieElement.innerHTML = pickflickaContent;
+    movieElement.innerHTML = "";
+    movieElement.innerHTML = pickflickaContent;
     return movieElement;
 }
 
@@ -41,113 +41,124 @@ $(searchButton).click(function () {
     // Get random number for page number between 1 and 10,000
     function pageNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-        var page = pageNumber(1, 500);
-        console.log(page);
+    }
+    var page = pageNumber(1, 500);
+    console.log(page);
     let randomURL = url + "discover/movie?api_key=a633b83aa763a0e8fad2c80cc66c54b9&language=en-US&page=" + page;
 
     fetch(randomURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        
-        genrePicker(data);
-        console.log("randomMovie", randomMovie);
-        
-        pickaflickContainer(randomMovie);
-        console.log(randomMovie.genre_ids);
-        console.log(randomMovie.vote_average);
-    })
-    .catch(function (error) {
-        console.error("Something has gone wrong");
-        console.error(error);
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+
+            genrePicker(data);
+            if (randomMovie === undefined) {
+                let errorElement = document.getElementById("randomMovieResult");
+                errorElementTemplate = 
+                `<div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <h5 class="randomMovieError">Oops, seems like I tripped. Please try again.</h5>
+                            </div>
+                        </div>
+                </div>`;
+                errorElement.innerHTML = "";
+                errorElement.innerHTML = errorElementTemplate;
+            }
+
+            console.log("randomMovie", randomMovie);
+
+            pickaflickContainer(randomMovie);
+            // console.log(randomMovie.genre_ids);
+            // console.log(randomMovie.vote_average);
+        })
+        .catch(function (error) {
+            console.error("Something has gone wrong");
+            console.error(error);
+
+        })
 })
 
 
-function genrePicker(data){
-    let movies = data.results;
-        let actionMovies = data.results.filter(function(movies){
-                return movies.genre_ids.includes(28);
-            })
-        let comedyMovies = data.results.filter(function(movies){
-                return movies.genre_ids.includes(35);
-            })
-        let dramaMovies = data.results.filter(function(movies){
-                return movies.genre_ids.includes(18);
-            })
-        let horrorMovies = data.results.filter(function(movies){
-                return movies.genre_ids.includes(27);
-            })
-        let scifiMovies = data.results.filter(function(movies){
-                return movies.genre_ids.includes(878);
-            })
-        let thrillerMovies = data.results.filter(function(movies){
-                return movies.genre_ids.includes(53);
-            })
-        
-        // Taget the genre dropdown and filter by genre id in the API 
-        if (document.getElementById("genre").value == "Action") {
-             randomMovie = actionMovies[Math.floor(Math.random() * actionMovies.length)];
-        }
-        else if (document.getElementById("genre").value == "Comedy") {
-            randomMovie = comedyMovies[Math.floor(Math.random() * comedyMovies.length)];
-        }
-        else if (document.getElementById("genre").value == "Drama") {
-            randomMovie = dramaMovies[Math.floor(Math.random() * dramaMovies.length)];
-        }
-        else if (document.getElementById("genre").value == "Horror") {
-            randomMovie = horrorMovies[Math.floor(Math.random() * horrorMovies.length)];
-        }
-        else if (document.getElementById("genre").value == "SciFi") {
-            randomMovie = scifiMovies[Math.floor(Math.random() * scifiMovies.length)];
-        }
-        else if (document.getElementById("genre").value == "Thriller") {
-            randomMovie = thrillerMovies[Math.floor(Math.random() * thrillerMovies.length)];
-        }
-        else{
-            randomMovie = movies[Math.floor(Math.random() * movies.length)];
-        }
+function genrePicker(data) {
+    let range = { min: 5.5, max: 10 }
+    data.results = data.results.filter(function (movies) {
+        return movies.vote_average <= range.max && movies.vote_average >= range.min;
+    })
+    let flicks = data.results;
+    let actionflicks = data.results.filter(function (movies) {
+        return movies.genre_ids.includes(28);
+    })
+    let comedyflicks = data.results.filter(function (movies) {
+        return movies.genre_ids.includes(35);
+    })
+    let dramaflicks = data.results.filter(function (movies) {
+        return movies.genre_ids.includes(18);
+    })
+    let horrorflicks = data.results.filter(function (movies) {
+        return movies.genre_ids.includes(27);
+    })
+    let scififlicks = data.results.filter(function (movies) {
+        return movies.genre_ids.includes(878);
+    })
+    let thrillerflicks = data.results.filter(function (movies) {
+        return movies.genre_ids.includes(53);
+    })
+
+    // Taget the genre dropdown and filter by genre id in the API 
+    if (document.getElementById("genre").value == "Action") {
+        randomMovie = actionflicks[Math.floor(Math.random() * actionflicks.length)];
+    }
+    else if (document.getElementById("genre").value == "Comedy") {
+        randomMovie = comedyflicks[Math.floor(Math.random() * comedyflicks.length)];
+    }
+    else if (document.getElementById("genre").value == "Drama") {
+        randomMovie = dramaflicks[Math.floor(Math.random() * dramaflicks.length)];
+    }
+    else if (document.getElementById("genre").value == "Horror") {
+        randomMovie = horrorflicks[Math.floor(Math.random() * horrorflicks.length)];
+    }
+    else if (document.getElementById("genre").value == "SciFi") {
+        randomMovie = scififlicks[Math.floor(Math.random() * scififlicks.length)];
+    }
+    else if (document.getElementById("genre").value == "Thriller") {
+        randomMovie = thrillerflicks[Math.floor(Math.random() * thrillerflicks.length)];
+    }
+    else {
+        randomMovie = flicks[Math.floor(Math.random() * flicks.length)];
+    }
 }
 
-function ratingPicker(data){
-    let belowFourRange = {min:1, max:4}
-    let fourSixRange = {min:4, max:6}
-    let sixEightRange = {min:6, max:8}
-    let aboveEightRange = {min:8, max:10}
-    // let ratedMovies = data.results.vote_average;
 
-    let belowFour = data.results.filter(function(movies){
-                return movies.vote_average <= belowFourRange.max && movies.vote_average >= belowFourRange.min;
-            })
-
-    let fourSix = data.results.filter(function(movies){
-                return movies.vote_average <= fourSixRange.max && movies.vote_average >= fourSixRange.min;
-            })
-
-    let sixEight = data.results.filter(function(movies){
-                return movies.vote_average <= sixEightRange.max && movies.vote_average >= sixEightRange.min;
-            })
-
-    let aboveEight = data.results.filter(function(movies){
-                return movies.vote_average <= aboveEightRange.max && movies.vote_average >= aboveEightRange.min;
-            })
-        
-        // Taget the rating dropdown and filter by vote_average in the API 
-        if (document.getElementById("rating").value == "Below 4") {
-            randomMovie = belowFour[Math.floor(Math.random() * belowFour.length)];
-        }
-        else if (document.getElementById("rating").value == "4-6") {
-            randomMovie = fourSix[Math.floor(Math.random() * fourSix.length)];
-        }
-        else if (document.getElementById("rating").value == "6-8") {
-            randomMovie = sixEight[Math.floor(Math.random() * sixEight.length)];
-        }
-        else if (document.getElementById("rating").value == "Above 8") {
-            randomMovie = aboveEight[Math.floor(Math.random() * aboveEight.length)];
-        }
-        else{
-            randomMovie = movies[Math.floor(Math.random() * movies.length)];
-        }
+function backgroundChange() {
+    if (document.getElementById("genre").value == "Any genre") {
+        $("body").css("background-color", "#e7e7eb");
+    }
+    if (document.getElementById("genre").value == "Action") {
+        $("body").css("background-color", "#bfcee1");
+    }
+    else if (document.getElementById("genre").value == "Comedy") {
+        $("body").css("background-color", "#ffffba");
+    }
+    else if (document.getElementById("genre").value == "Drama") {
+        $("body").css("background-color", "#ff9aa2");
+    }
+    else if (document.getElementById("genre").value == "Horror") {
+        $("body").css("background-color", "#b5ead7");
+    }
+    else if (document.getElementById("genre").value == "SciFi") {
+        $("body").css("background-color", "#d8d8d8");
+    }
+    else if (document.getElementById("genre").value == "Thriller") {
+        $("body").css("background-color", "#ffdac1");
+    }
+    else {
+        $("body").css("background-color", "#e7e7eb");
+    }
 }
+
+
+
+
