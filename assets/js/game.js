da47b8913d72a88d74f3250d0ae164a17a11e594
vishkid84 <vishalittr@gmail.com>
2020-04-20 21:38:52 +0000

    Moved scripts to click function to randomize appearance of questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,10 +10,7 @@ $(document).ready(function(){
         questionIndexNumber.innerHTML = questionIndex;
     let questionElement = document.getElementById("question");
     let answerOne = document.getElementById("answer1");
-    let answerTwo = document.getElementById("answer2");
-    let answerElements = [answerOne, answerTwo];
-    // Alternate randomly between two options
-    let randomAnswerElement = answerElements[Math.floor(Math.random() * answerElements.length)];
+    
    
 
     $(".startBtn").on("click",function(){
@@ -27,7 +24,10 @@ $(document).ready(function(){
             }
             var page = pageNumber(1, 500);
             console.log(page);
-                
+        let answerTwo = document.getElementById("answer2");
+        let answerElements = [answerOne, answerTwo];
+        // Alternate randomly between two options
+        let randomAnswerElement = answerElements[Math.floor(Math.random() * answerElements.length)];
         let randomURL = url + "discover/movie?api_key=a633b83aa763a0e8fad2c80cc66c54b9&language=en-US&page=" + page;
         
 
@@ -55,68 +55,72 @@ $(document).ready(function(){
                 console.error("Something has gone wrong");
                 console.error(error);
             })
-    }) 
 
-
-    
-    function showQuestionAnswer(movies){
+        function showQuestionAnswer(movies){
         
-        let randomMovie = movies[Math.floor(Math.random() * movies.length)];
-        questionElement.innerHTML = randomMovie.overview;
+            let randomMovie = movies[Math.floor(Math.random() * movies.length)];
+            questionElement.innerHTML = randomMovie.overview;
 
-        // Answer array
-        // let wrongAnswer = questions[0].answers;
+            // Answer array
+            // let wrongAnswer = questions[0].answers;
 
-        // get random wrong movie from array
-        let randomWrongAnswer = movies[Math.floor(Math.random() * movies.length)];
 
-        // get right movie title from json
-        let randomRightAnswer = randomMovie.title;     
+            // get random wrong movie from array
+            let randomWrongAnswer = movies[Math.floor(Math.random() * movies.length)];
 
-        randomAnswerElement.innerHTML = randomWrongAnswer.title; //returns wrong answer
-        if (randomAnswerElement == answerOne) {
-            answerTwo.innerHTML = randomRightAnswer;
-        }else {
-            answerOne.innerHTML = randomRightAnswer;
+            // get right movie title from json
+            let randomRightAnswer = randomMovie.title;     
+
+            randomAnswerElement.innerHTML = randomWrongAnswer.title; //returns wrong answer
+            if (randomAnswerElement == answerOne) {
+                answerTwo.innerHTML = randomRightAnswer;
+            }else {
+                answerOne.innerHTML = randomRightAnswer;
+            }
+            checkAnswer(randomRightAnswer)
         }
-        checkAnswer(randomRightAnswer)
-    }  
 
-    function checkAnswer(randomRightAnswer){
-        let answerClass = document.getElementById("answer");
-        Array.from(answerClass.children).forEach(answer => {
-            $(answer).click(function(){
-                if (this.innerText == randomRightAnswer) {
-                    $(this).addClass("rightAnswer");
-                    $(this).removeClass("wrongAnswer");
-                    score +=5;
-                    console.log(score);
-                    let scoreValue = document.getElementById("scoreResult");
-                        scoreValue.innerHTML = score;
-                } else {
-                    $(this).addClass("wrongAnswer");
-                }
+        function checkAnswer(randomRightAnswer){
+            let answerClass = document.getElementById("answer");
+            Array.from(answerClass.children).forEach(answer => {
+                $(answer).click(function(){
+                    if (this.innerText == randomRightAnswer) {
+                        $(this).addClass("rightAnswer");
+                        $(this).removeClass("wrongAnswer");
+                        score +=5;
+                        console.log(score);
+                        let scoreValue = document.getElementById("scoreResult");
+                            scoreValue.innerHTML = score;
+                    } else {
+                        $(this).addClass("wrongAnswer");
+                    }
+                })
+            });
+        }
+
+        function nextClick (){
+            $(".nextButton").click(function(){
+                clearForms();
+                $(".custom-control").removeClass("rightAnswer");
+                $(".custom-control").removeClass("wrongAnswer");
             })
-        });
-    }
+        }
 
-    function nextClick (){
-        $(".nextButton").click(function(){
-            clearForms();
-            $(".custom-control").removeClass("rightAnswer");
-            $(".custom-control").removeClass("wrongAnswer");
+        // Reset quiz
+        $(".resetQuizLink").click(function(){
+            $(location).attr('href',"flickquiz.html");
         })
-    }
 
-    $(".resetQuizLink").click(function(){
-        $(location).attr('href',"flickquiz.html");
-    })
-
-    function clearForms()
-    {
-        $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
-        $(':checkbox, :radio').prop('checked', false);
-    }
+        $(".custom-radio").click(function(){
+            $(this).children().prop("checked", true);
+        });
+        
+        function clearForms()
+        {
+            $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
+            $(':checkbox, :radio').prop('checked', false);
+        }
+    }) 
 
 })
 
